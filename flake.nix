b538c92effe97473e88c7b938e3c48c80ad5aeea
Charles Ellis <hamled@hamled.dev>
2023-08-28 18:26:31 -0700

    Define package output

--- flake.nix
+++ flake.nix
@@ -3,5 +3,33 @@
 
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: let system = "x86_64-linux"; in { };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+
+      jumpcloud-pw = with pkgs;
+        appimageTools.wrapType2 {
+          name = "jumpcloud-pw";
+          src = fetchurl {
+            url =
+              "https://cdn.pwm.jumpcloud.com/DA/release/JumpCloud-Password-Manager-latest.AppImage";
+            hash = "sha256-/N94SH3PamM65537iIWn7XhjcZI2luFVLCqmKtpRG8Y=";
+          };
+          extraPkgs = pkgs: with pkgs; [ libsecret ];
+
+          meta = with lib; {
+            description = "JumpCloud Password Manager";
+            homepage = "https://jumpcloud.com/platform/password-manager";
+            license = licenses.unfree;
+            maintainers = [{
+              name = "Charles Ellis";
+              email = "hamled@hamled.dev";
+              github = "Hamled";
+              githubId = 66355;
+            }];
+            platforms = [ "x86_64-linux" ];
+          };
+        };
+    in { packages.${system}.default = jumpcloud-pw; };
 }
